775615ec199063194debde36ddc6069e6b8e698d
LDQ <liudqliu94@gmail.com>
2020-04-24 23:03:48 +0800

    mybatis add if, where, choose, bind, foreach tag

--- mybatis/src/com/ldq/TestMybatis.java
+++ mybatis/src/com/ldq/TestMybatis.java
@@ -2,8 +2,11 @@ package com.ldq;
  
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
- 
+import java.util.Map;
+
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -19,10 +22,10 @@ public class TestMybatis {
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         try (SqlSession session = sqlSessionFactory.openSession()) {
-	        List<Category> cs = session.selectList("listCategory");
-	        for(Category c : cs) {
-	            System.out.println(c);
-	        }
+//	        List<Category> cs = session.selectList("listCategory");
+//	        for(Category c : cs) {
+//	            System.out.println(c);
+//	        }
 
         	List<Product> result  = session.selectList("selectAll");
         	for (Product p : result) {
@@ -30,11 +33,43 @@ public class TestMybatis {
         	}
         	
         	
-        	// 模糊查询
-        	List<Category> cFuzzy = session.selectList("selectCategoryByNameFuzzy", "up");
-        	for (Category c : cFuzzy) {
-        		System.out.println(c);
+        	System.out.println("tag test");
+        	
+//        	Map<String, Object> paramsUpdate = new HashMap<>();
+//        	paramsUpdate.put("name", "xx");
+//        	paramsUpdate.put("price", "800");
+//        	paramsUpdate.put("id", "6");
+//        	session.update("updateProductUseTrim", paramsUpdate);
+        	
+//        	Map<String, Object> params = new HashMap<>();
+//        	params.put("name", "x");
+//        	List<Product> resultFuzzy = session.selectList("selectAllWithChoose", params);
+//        	for (Product p : resultFuzzy) {
+//        		System.out.println(p);
+//        	}
+        	
+//        	List<Integer> ids = new ArrayList<>();
+//        	ids.add(1);
+//        	ids.add(2);
+//        	List<Product> prange = session.selectList("selectInRange", ids);
+//        	for(Product p : prange) {
+//        		System.out.println(p);
+//        	}
+        	
+        	Map<String, Object> params = new HashMap<>();
+        	params.put("name", "x");
+        	List<Product> pBind = session.selectList("selectWithBind", params);
+        	for (Product p : pBind) {
+        		System.out.println(p);
         	}
+        	
+        	
+//        	// 模糊查询
+//        	List<Category> cFuzzy = session.selectList("selectCategoryByNameFuzzy", "up");
+//        	for (Category c : cFuzzy) {
+//        		System.out.println(c);
+//        	}
+        	session.commit();
         }
          
     }
